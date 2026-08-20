dd006908d833c61ccd7fecf177e95db64b21335d
RyanTran07 <rtran934@gmail.com>
2026-08-20 01:37:17 -0700

    Backend now supports adding more books.

--- script.js
+++ script.js
@@ -1,43 +1,34 @@
-
+"use strict";
 // Event Listeners
 const newBookButton = document.querySelector(".new-book-btn");
 const booksContainer = document.querySelector(".books-container");
-
-const books = []
-
-function Book(title: string, author: string, pages: number, isRead: boolean, bookID: string) {
+const books = [];
+function Book(title, author, pages, isRead, bookID) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
     this.bookID = bookID;
 }
-
-function addBookToLibrary(title: string, author: string, pages: number, isRead: boolean) {
+function addBookToLibrary(title, author, pages, isRead) {
     const bookID = crypto.randomUUID();
-
     // Create the book and then add it to the array
     const newBook = new Book(title, author, pages, isRead, bookID);
-    books.push(newBook)
-
+    books.push(newBook);
     // Modify the DOM by adding the new book as a card to the book container.
     const bookCard = createBookCard(newBook);
     booksContainer?.appendChild(bookCard);
 }
-
-function createBookCard(book: Book) {
+function createBookCard(book) {
     const card = document.createElement("div");
     card.classList.add("book-card");
     card.dataset.bookID = book.bookID;
-
     card.innerHTML = `
         <div class="book-title">${book.title}</div>
         <div class="book-author">Author: ${book.author}</div>
         <div class="book-pages">Pages: ${book.pages}</div>
         <div class="book-isRead">Read: ${book.isRead ? "Yes" : "No"}</div>
     `;
-
     return card;
 }
-
 addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false);
